c80325bc63e31b309e63ea6c122216de47a759ec
Jingchen Hu <hjc.noname@gmail.com>
2016-05-22 22:24:44 -0700

    generate bitext

--- pinyin2chars.py
+++ pinyin2chars.py
@@ -1,17 +1,55 @@
 import sqlite3
+import collections
+import copy
 
 import sqlqueries
 
 connection = sqlite3.connect('data/lcmc.db3')
 cursor = connection.cursor()
 
-# bitext: list(list(str)), a list of sentences. Each str has the form: "char#pinyin"
+
+def cid_to_sid(cid):
+    return cid[:cid.index("-")]
+
+def format_cid(ids):
+    word_num = str(ids[2]).zfill(3)
+    char_num = str(ids[3]).zfill(2)
+    return "{}.{}-{}:{}".format(str(ids[0]), str(ids[1]), word_num, char_num)
+
+def format_pair(chars):
+    return chars[0] + u"#" + unicode(chars[1])
+
+# Bitext: list(list(str)), a list of text segments.
+# Each str has the form: "char#pinyin", or "x#x" where token_type of x != 'w'
 def get_bitext_corpus():
-    cursor.execute(sqlqueries.GET_BITEXT_QUERY)
-    raw_res = cursor.fetchall()
-    return raw_res
+    # Query returns a list of tuples (cid, "char#pinyin"). Punctuations excluded.
+    # cid = "file_id.sentence_id.word_num.char_num"
+    cid_map = {}
+    for tup in cursor.execute(sqlqueries.GET_BITEXT):
+    	cid_map[format_cid(tup[0:4])] = format_pair(tup[4:6])
+    # for nonchars value is "x#x"
+    nonchar_cids = set()
+    for tup in cursor.execute(sqlqueries.GET_NONCHARS):
+        cid = format_cid(tup[0:4])
+    	cid_map[cid] = format_pair(tup[4:6])
+    	nonchar_cids.add(cid)
+    res = []
+    cur_segment = []
+    last_sid = None
+    for cid in sorted(cid_map.keys()):
+        if cur_segment and ((cid in nonchar_cids) or (last_sid and last_sid != cid_to_sid(cid))):
+            res.append(copy.deepcopy(cur_segment))
+            cur_segment = []
+            last_sid = None
+        if not cid in nonchar_cids:
+            cur_segment.append(cid_map[cid])
+            last_sid = cid_to_sid(cid)           
+    if (cur_segment):
+        res.append(copy.deepcopy(cur_segment))
+    return res
 
 bitext = get_bitext_corpus()
 
-print bitext
+for i in range(0,20):
+    print u" ".join(bitext[i])
 
--- sqlqueries.py
+++ sqlqueries.py
@@ -1,5 +1,16 @@
+# hardcoded text type for now
 
-GET_BITEXT_QUERY = '''SELECT c.file_id || "." || c.sentence_id, c.character,  pc.character
-    FROM characters c JOIN pinyin_characters pc USING (file_id, sentence_id, word_num, char_num)
-    WHERE c.text_id in ("M")'''
+GET_BITEXT = '''
+        SELECT c.file_id, c.sentence_id, c.word_num, c.char_num,
+            c.character, pc.character
+        FROM characters c JOIN pinyin_characters pc
+            USING (file_id, sentence_id, word_num, char_num)
+        WHERE c.text_id in ("B")
+    '''
 
+GET_NONCHARS = '''
+        SELECT c.file_id, c.sentence_id, c.word_num, c.char_num,
+            c.character, c.character
+        FROM characters c
+        WHERE c.text_id in ("B") and c.token_type != "w"
+    '''
