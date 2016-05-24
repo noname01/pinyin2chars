60e93d2c5b0bac0c52a063f33b5948e91420a34d
Jingchen Hu <hjc.noname@gmail.com>
2016-05-24 14:09:20 -0700

    add n-gram counts

--- pinyin2chars.py
+++ pinyin2chars.py
@@ -1,6 +1,7 @@
-import sqlite3
 import collections
 import copy
+
+import sqlite3
 
 import sqlqueries
 
@@ -19,22 +20,28 @@ def format_cid(ids):
 def format_pair(chars):
     return chars[0] + u"#" + unicode(chars[1])
 
-# Bitext: list(list(str)), a list of text segments.
-# Each str has the form: "char#pinyin", or "x#x" where token_type of x != 'w'
+# Bitext: list(list(str)), a list of text segments / utterances.
+# Each str has the form: "char#pinyin", or "x#x" where is_cjk of x = "N"
 def get_bitext_corpus():
+    print "Loading bitext..."
     # Query returns a list of tuples (cid, "char#pinyin"). Punctuations excluded.
     # cid = "file_id.sentence_id.word_num.char_num"
     cid_map = {}
+    training_chars = set()
     for tup in cursor.execute(sqlqueries.GET_BITEXT):
     	cid_map[format_cid(tup[0:4])] = format_pair(tup[4:6])
+        training_chars.add(tup[4])
     # Query returns non-cjk chars. The value is "x#x".
     nonchar_cids = set()
+    
     for tup in cursor.execute(sqlqueries.GET_NONCHARS):
         cid = format_cid(tup[0:4])
     	cid_map[cid] = format_pair(tup[4:6])
     	# Special symbols like numbers where token_type = w should be added.
         if (tup[6] != "w"):
             nonchar_cids.add(cid)
+        else:
+            training_chars.add(tup[4]);
     res = []
     cur_segment = []
     last_sid = None
@@ -48,12 +55,36 @@ def get_bitext_corpus():
             last_sid = cid_to_sid(cid)           
     if (cur_segment):
         res.append(copy.deepcopy(cur_segment))
+    # print u" ".join(training_chars)
+    print "Done."
+    print "{} text segments parsed.".format(str(len(res)))
+    print "{} unique characters found.".format(str(len(training_chars)))
     return res
 
-print "Loading bitext..."
+# text: list(list(tokens))
+# Generates a dictionary that maps n-grams to counts.
+def get_ngram_counts(text, n):
+    print "Generating {}-gram model...".format(str(n))
+    ngram_counts = {}
+    for segment in text:
+        # add start and end symbols
+        line = copy.deepcopy(segment)
+        line.insert(0, "<s>")
+        line.insert(len(line), "</s>")
+        head = 0
+        while (head + n <= len(line)):
+            gram = u" ".join(line[head:head+n])
+            if not gram in ngram_counts:
+                ngram_counts[gram] = 0
+            ngram_counts[gram] += 1
+            head += 1
+    print "Done."
+    return ngram_counts
+
+def smooth_counts(counts):
+    return counts
+
 bitext = get_bitext_corpus()
-print "Done."
 
-for i in range(0,20):
-    print u" ".join(bitext[i])
-
+unigram_counts = smooth_counts(get_ngram_counts(bitext, 1))
+bigram_counts = smooth_counts(get_ngram_counts(bitext, 2))
--- sqlqueries.py
+++ sqlqueries.py
@@ -5,12 +5,12 @@ GET_BITEXT = '''
             c.character, pc.character
         FROM characters c JOIN pinyin_characters pc
             USING (file_id, sentence_id, word_num, char_num)
-        WHERE c.text_id in ("B")
+        WHERE c.text_id in ("M")
     '''
 
 GET_NONCHARS = '''
         SELECT c.file_id, c.sentence_id, c.word_num, c.char_num,
             c.character, c.character, c.token_type
         FROM characters c
-        WHERE c.text_id in ("B") and c.is_cjk = "N"
+        WHERE c.text_id in ("M") and c.is_cjk = "N"
     '''
